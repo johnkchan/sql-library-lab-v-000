dd2aa374c1a265d2437f522ff25c99ee3620d607
Jonathan Chan <johnkchan252@gmail.com>
2018-08-22 00:41:59 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,8 +1,14 @@
-CREATE TABLE Series(
+CREATE TABLE series(
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
   funding_goal INTEGER,
   start_date TEXT,
   end_date TEXT
+);
+
+CREATE TABLE Sub-Genre(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
 );
