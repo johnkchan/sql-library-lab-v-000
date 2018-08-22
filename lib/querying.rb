01446179577b7ec490b08f2fc2474093d1737202
Jonathan Chan <johnkchan252@gmail.com>
2018-08-22 01:44:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,8 +35,8 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "Write your SQL query here"
-  "SELECT c.name
+  "SELECT c.name, count(character_books.book_id
   FROM characters as c
   INNER JOIN character_books on c.id = character_books.character_id
-  GROUP BY c.name, character_books.book_id"
+  GROUP BY c.name, "
 end
