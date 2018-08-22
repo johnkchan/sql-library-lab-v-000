6a425af20dd92f151c977f4fc41e25a5e038699e
Jonathan Chan <johnkchan252@gmail.com>
2018-08-22 01:21:55 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,6 +24,9 @@ end
 
 def select_series_title_with_most_human_characters
   "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  INNER JOIN characters as c ON c.series_id = series.id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
