c5d05d9e0aadfabab1eab4e92722817c68a76231
Jonathan Chan <johnkchan252@gmail.com>
2018-08-22 01:34:09 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
   INNER JOIN characters as c ON c.series_id = series.id
-  GROUP BY c.series_id, c.species"
+  GROUP BY c.series_id, c.species
+  ORDER BY COUNT(c.species)"
 end
 
 def select_character_names_and_number_of_books_they_are_in
