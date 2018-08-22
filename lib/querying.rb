0c0946234b0fbb07202eef60ee102467ea6ac229
Jonathan Chan <johnkchan252@gmail.com>
2018-08-22 01:09:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT author.name, series.subgenre_id
   FROM author
   INNER JOIN series
-  ON "
+  ON author.id = series.author_id"
 end
 
 def select_series_title_with_most_human_characters
